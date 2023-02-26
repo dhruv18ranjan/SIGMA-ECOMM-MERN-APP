193bf4f18f1d2b5da4107336da1b06a766a85d41
dhruv <dhruv18ranjan@gmail.com>
2023-02-27 00:56:54 +0530

    buikd

--- Backend/index.js
+++ Backend/index.js
@@ -36,6 +36,8 @@ if(process.env.NODE_ENV==='production'){
     app.use(express.static("design/build"));
 }
 
-app.listen(5000,()=>{
+const PORT= process.env.PORT || 5000;
+
+app.listen(PORT,()=>{
     console.log("backend server is running ");
 })
